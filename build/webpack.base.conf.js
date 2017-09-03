832ec17b5e37243b601a4c08b9bebc67b135cc25
wangzhen-CHN <137014311@qq.com>
2017-09-03 14:57:11 +0800

    123

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -14,10 +14,7 @@ module.exports = {
   },
   output: {
     path: config.build.assetsRoot,
-    filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    filename: '[name].js'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
